29f194c8ea662e76fc1f069d7a738479927bd253
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-14 09:19:19 +0300

    Java Lambda Functions

--- src/jl004_streamprojects/JavaStreamsAPI001.java
+++ src/jl004_streamprojects/JavaStreamsAPI001.java
@@ -8,10 +8,8 @@ public class JavaStreamsAPI001 {
         IntStream stream = Arrays.stream(new int[]{1, 2, 3, 4, 5});//IntStream stream = IntStream.of(1, 2, 3, 4, 5);
         stream.forEach(System.out::println);
 
-
         IntStream stream1 = IntStream.rangeClosed(1, 10);
         stream1.forEach(System.out::println);
-
 
         IntStream stream2 = IntStream.range(1, 10);
         stream2.forEach(System.out::println);
@@ -24,5 +22,20 @@ public class JavaStreamsAPI001 {
 
         IntStream stream5 = IntStream.range(1, 10).map(x -> x * x).filter(x -> x % 2 == 0).limit(5);
         stream5.forEach(System.out::println);
+
+        IntStream stream6 = IntStream.range(1, 10).map(x -> x * x).filter(x -> x % 2 == 0).skip(5);
+        stream6.forEach(System.out::println);
+
+        IntStream stream7 = IntStream.range(1, 10).map(x -> x * x).filter(x -> x % 2 == 0).limit(5).skip(5);
+        stream7.forEach(System.out::println);
+
+        IntStream stream8 = IntStream.range(1, 10).map(x -> x * x).filter(x -> x % 2 == 0).skip(5).limit(5);
+        stream8.forEach(System.out::println);
+
+        IntStream stream9 = IntStream.range(1, 10).map(x -> x * x).filter(x -> x % 2 == 0).limit(5).skip(5).limit(5);
+        stream9.forEach(System.out::println);
+
+        IntStream stream10 = IntStream.range(1, 10).map(x -> x * x).filter(x -> x % 2 == 0).skip(5).limit(5).skip(5);
+        stream10.forEach(System.out::println);
     }
 }
